bf6ca26f44f5a4621e8c6248a336ed232c92610c
shivam verma <88495364+Verma051@users.noreply.github.com>
2023-08-14 21:20:12 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,99 @@
+// alert("hello world")
+// console.log("hello world")
+// console.error("this is an error")
+// console.warn("this is an warning")
+// const age = 30;
+// console.log(age)
+// let age = 30
+// age = 31
+// console.log(age)
+// const name = 'john'
+// const age = 31
+// const isCOOL = true
+// const rating  = 4.5
+// const x  = null
+// const y  = undefined
+// let z;
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isCOOL)
+// console.log(typeof rating)
+// console.log(typeof x)
+// console.log(typeof y)
+// // console.log(typeof z)
+//  const name = 'shivam'
+// const age = 22;
+// console.log(` my name is ${name} and i am ${age}`);
+//  const s = 'shivam'
+//  console.log(s.length)
+//  console.log(s.toUpperCase())
+//  console.log(s.toLowerCase())
+//  console.log(s.substring(0,3))
+//  console.log(s.substring(0,3).toUpperCase)
+//  console.log(s.split(''))
+//  console.log(s.split(' ,'))
+// const numbers = new Array(1,2,3,4,5,6,7,8)
+// console.log(numbers)
+// const fruits = ['apple','oranges','pears','banana']
+// console.log(fruits)
+// console.log(fruits[1])
+// fruits [4] = 'mangoes'
+// console.log(fruits)
+// fruits.push('grapes')
+// console.log(fruits)
+// fruits.unshift('strawberry')
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits)
+// console.log(Array.isArray(fruits))
+// console.log(Array.isArray('hello'))
+// console.log(fruits.indexOf('oranges'))
+// const person ={
+//     firstName : 'shivam',
+//     lastName : 'verma',
+//     age : 30,
+//     hobbies : ['music','movies','sports'],
+//     address  :{ 
+//         street :'50 main course',
+//         city : 'boston',
+//         state :'MA',
+//     }
+// }
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.firstName,person.lastName);
+// console.log(person.hobbies[1]);
+// console.log(person.hobbies);
+// console.log(person.address.street);
+// person.email = "shivamverma1270@gmail.com"
+// console.log(person);
+// const toDos = [
+//     {
+//         id : 1,
+//         text : 'take care',
+//         isComplete : true,
+//     },
+//     {
+//         id : 2,
+//         text : 'meeting with boss',
+//         isComplete : false,
+//     },
+//     {
+//         id : 3,
+//         text : 'dentist appointment',
+//         isComplete : true,
+//     }
+// ];
+// console.log(toDos)
+// console.log(toDos[1].text)
+// for(i=0;i<=10;i++)
+// {
+// console.log(`for loop numbers ${i}`)
+// 
+let i=0;
+while(i<=10)
+{
+    console.log(`while loop numbers ${i}`)
+    i++;
+}
